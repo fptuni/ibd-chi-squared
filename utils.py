f3b4ec44a24874e6b8a04a0ea20303cec43a95ad
Tran Thanh Trong Hub <trongttce130169@gmail.com>
2020-12-13 13:38:09 +0700

    Add export excel

--- utils.py
+++ utils.py
@@ -6,6 +6,65 @@ from numpy.random import default_rng
 from sklearn import metrics
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import MinMaxScaler
+import xlsxwriter
+import pandas as pd
+
+
+# creating the DataFrame
+def exportResults(file_name, file_train_name, list_test_file_name, result_list_in_orders):
+    number_of_rows = len(result_list_in_orders)
+    number_of_test = len(list_test_file_name)
+    if (len(list_test_file_name) != 5):
+        print("Sorry! We are not implement auto numbber of tex")
+    workbook = xlsxwriter.Workbook(file_name)
+    worksheet = workbook.add_worksheet(name="Chi-Squared")
+    # Increase the cell size of the merged cells to highlight the formatting.
+    worksheet.set_column('A:C')
+    worksheet.set_column('A:B', 60)
+    worksheet.set_column('C:C',40)
+    # Create a format to use in the merged range.
+    merge_format = workbook.add_format({
+        'bold': 1,
+        'border': 1,
+        'align': 'center',
+        'valign': 'vcenter',
+        'fg_color': 'yellow'})
+    merge_format_2 = workbook.add_format({
+        'bold': 1,
+        'border': 1,
+        'align': 'center',
+        'font_color': 'white',
+        'valign': 'vcenter',
+        'fg_color': 'green'})
+    merge_format_3 = workbook.add_format({
+        'font_color': 'green'
+    })
+    merge_format_4 = workbook.add_format({
+        'bottom': 1,
+        'left': 1
+    })
+    merge_format_2.set_text_wrap()
+    merge_format.set_text_wrap()
+    # Merge 3 cells.
+    worksheet.merge_range('A1:A' + str(number_of_rows), "Train by " + file_train_name, merge_format_2)
+    # Merge 3 cells over two rows.
+    count_test = 0
+    for x in range(number_of_test):
+        first_row = x * 6 + 1
+        last_row = x * 6 + 6
+        worksheet.conditional_format('C' + str(last_row) + ':C' + str(last_row), {'type': 'no_blanks', 'format': merge_format_4})
+        if (count_test <= (len(list_test_file_name) - 1)):
+            worksheet.merge_range('B' + str(first_row) + ':B' + str(last_row),
+                                  "Test on " + list_test_file_name[count_test], merge_format)
+            count_test += 1
+
+    for x in range(number_of_rows):
+        if "Random" in result_list_in_orders[x]:
+            worksheet.write('C'+str(x+1),result_list_in_orders[x])
+        else:
+            worksheet.write('C'+str(x+1),result_list_in_orders[x],merge_format_3)
+    workbook.close()
+    print('Data is written to excel File.')
 
 
 class TeamFile:
@@ -23,11 +82,13 @@ def getNewDataset():
     train = os.path.join(dirname, 'data/ibdfullHS_iCDf_x.csv')  # iCDr & UCf &iCDf &CDr&CDf ibdfullHS_iCDf_x
     fileListTest = []
     fileListTest.append(os.path.join(dirname, 'data/ibdfullHS_iCDr_x.csv'))
-    # fileListTest.append(os.path.join(dirname, 'data/ibdfullHS_UCf_x.csv'))
     fileListTest.append(os.path.join(dirname, 'data/ibdfullHS_CDr_x.csv'))
     fileListTest.append(os.path.join(dirname, 'data/ibdfullHS_CDf_x.csv'))
     fileListTest.append(os.path.join(dirname, 'data/ibdfullHS_UCr_x.csv'))
+    fileListTest.append(os.path.join(dirname, 'data/ibdfullHS_UCf_x.csv'))
     return TeamFile(train, fileListTest, "RS")
+
+
 def findImportancesFeatures(resultColName, filenameTrain, coef_percent, nlargestFeatures, num_feats):
     print("Feature selection method to be used : " + "Chi-Squared")
     print(str("Train by file ") + str(filenameTrain))
@@ -87,7 +148,6 @@ def sumThenAveragePercisely(accuracy_model_acc):
 
 # Train on one dataset, then test of another dataset
 def subteam2(filenameTrain, resultColName, fileListTest, nTimes, coef_percent, nlargestFeatures):
-
     importanceFeature, X_Train_ImportFeature, y_Train_ImportFeature = findImportancesFeatures(resultColName,
                                                                                               filenameTrain,
                                                                                               coef_percent,
@@ -106,12 +166,13 @@ def subteam2(filenameTrain, resultColName, fileListTest, nTimes, coef_percent, n
     mcc_if = 0.0
     auc_if = 0.0
     print("Results begin ----------------- ")
+    resultSetForExcel = []
     for x in range(len(fileListTest)):
-        print("Run test on the file name " + fileListTest[x] + ". Repeat " + str(nTimes) + " time(s).")
+        print("Run test on the file name " + fileListTest[x])
         for n in range(nTimes):
             if nTimes == 0:
                 break
-            #Get data from file to test
+            # Get data from file to test
             data_yu = pd.read_csv(fileListTest[x])
 
             # Get the test data same column name with Random for compare
@@ -126,7 +187,7 @@ def subteam2(filenameTrain, resultColName, fileListTest, nTimes, coef_percent, n
             mcc_random += metrics.matthews_corrcoef(y_Test_Random, y_Pred_Random)
             auc_random += metrics.roc_auc_score(y_Test_Random, y_Pred_Random)
 
-            #Get the test data same column name with IF
+            # Get the test data same column name with IF
             df_IF = pd.DataFrame(data_yu, columns=importanceFeature).fillna(0)
             X_Test_IF = df_IF[importanceFeature]
             y_Test_IF = data_yu[resultColName]
@@ -141,9 +202,18 @@ def subteam2(filenameTrain, resultColName, fileListTest, nTimes, coef_percent, n
             if nTimes == 0:
                 break
         printResult(acc_random, mcc_random, auc_random, acc_if, mcc_if, auc_if, nTimes)
+        resultSetForExcel.append("Random ACC = " + str(acc_random));
+        resultSetForExcel.append("Random MCC = " + str(mcc_random));
+        resultSetForExcel.append("Random AUC = " + str(auc_random));
+        resultSetForExcel.append("IF ACC= "+ str(acc_if));
+        resultSetForExcel.append("IF MCC= "+ str(mcc_if));
+        resultSetForExcel.append("IF AUC= "+ str(auc_if));
         acc_if = 0.0
         mcc_if = 0.0
         auc_if = 0.0
         acc_random = 0.0
         mcc_random = 0.0
         auc_random = 0.0
+    import time
+    file_excel_name = "Result-" + time.strftime("%d%B%Y-%Hh%Mm%Ss") + ".xlsx"
+    exportResults(file_excel_name, filenameTrain, fileListTest, resultSetForExcel)
