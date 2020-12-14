7d5b4524b2397001d662aa0aa95c0ed995a9e670
Tran Thanh Trong Hub <trongttce130169@gmail.com>
2020-12-14 16:58:45 +0700

    Final results

--- main.py
+++ main.py
@@ -4,5 +4,13 @@ print("========= Stimulates Start =========")
 print("Algorithms use: ")
 print("Chi-Squared")
 team_file = getNewDataset()
-subteam2(team_file.train, team_file.resultColName,
-         team_file.listFileTest, 1, 0.1, 25)
+for x in range(len(team_file.listFileTest) + 1):
+    if(x!=0):
+        tmp_name = team_file.train
+        team_file.train = team_file.listFileTest[x-1]
+        team_file.listFileTest[x - 1] = tmp_name
+        subteam2(team_file.train, team_file.resultColName,
+                 team_file.listFileTest, 1, 0.1, 25)
+    else:
+        subteam2(team_file.train, team_file.resultColName,
+                 team_file.listFileTest, 1, 0.1, 25)
